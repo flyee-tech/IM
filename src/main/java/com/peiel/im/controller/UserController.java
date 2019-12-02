31d0fc217fa604da6eff074073c7f632f1fa22cc
peierlong <peielcn@gmail.com>
2019-12-02 14:20:50 +0800

    update login interface to post

--- src/main/java/com/peiel/im/controller/UserController.java
+++ src/main/java/com/peiel/im/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
@@ -34,7 +34,7 @@ public class UserController {
         }
     }
 
-    @RequestMapping(path = "/login")
+    @PostMapping(path = "/login")
     public String login(@RequestParam String userName, @RequestParam String password, Model model, HttpSession session) {
         try {
             UserDO loginUserDO = userService.login(userName, password);
